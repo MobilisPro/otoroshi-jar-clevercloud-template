b538dcb40f8bf618a1047546f98ef0ea66246744
Yann Danthu <yann@danthu.com>
2018-11-08 16:28:21 +0100

    Upgrade to otoroshi 1.3.1

--- clevercloud/build.sh
+++ clevercloud/build.sh
@@ -2,4 +2,4 @@
 
 echo "Run build from CC hooks"
 
-wget -q --show-progress 'https://dl.bintray.com/maif/binaries/otoroshi.jar/1.2.0/otoroshi.jar'
+wget -q --show-progress 'https://dl.bintray.com/maif/binaries/otoroshi.jar/1.3.1/otoroshi.jar'
